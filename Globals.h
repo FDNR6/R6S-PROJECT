85c1728d96e475125fb846657514b923eb5e531d
FDN <65097655+FDNR6@users.noreply.github.com>
2020-09-11 17:58:00 -0400

    Add files via upload

---
+++ Globals.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace Globals
+{
+ extern bool glow;
+ extern float GlowCol[3];
+ extern float RGBSpeed;
+ extern bool GlowRGB;
+ extern bool hands;
+ extern bool norecoil;
+ extern bool nospread;
+ extern bool noclip;
+ extern bool speed;
+ extern bool rapidfire;
+ extern bool cavesp;
+ extern bool instantanimation;
+ extern bool unlockall;
+ extern bool glowStyle;
+ extern bool bulletMod;
+ extern bool long_knife;
+ extern bool get_localplayer;
+ extern bool RunShoot;
+}
